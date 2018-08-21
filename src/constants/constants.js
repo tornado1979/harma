469a55e34783579d3613716b25135b3142942400
Theodoros Itzaris <theodoreme@gmail.com>
2018-08-22 01:28:12 +0300

    Create mock server on postman and fetch the json data from there

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,1 @@
-export const DATA_API_ENDPOINT = 'http://www.mocky.io/v2/5b7892872e00006900864cc0'
+export const DATA_API_ENDPOINT = 'https://a0835f56-83a4-4eda-86a2-41ea527122aa.mock.pstmn.io/data'
